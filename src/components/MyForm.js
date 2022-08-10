d4c8e1b4a55307772a91a4e6bace895772440e6f
TorugoJS <victor.devfrontend@gmail.com>
2022-08-10 20:31:08 -0300

    Forms

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -3,14 +3,22 @@ import './MyForm.css'
 const MyForm = () => {
     return (
         <div>
-            {/* 1 - Criação de form */}
 
+            {/* 1 - Criação de form */}
             <form>
                 <div>
                     <label htmlFor="name">Nome:</label>
                     <input type="text" name="name" placeholder="Insira seu nome" />
                 </div>
-            <input type="submit" value="Enviar"/>
+
+                {/* Label envolvendo input */}
+                <label>
+                    <span>E-mail</span>
+                    <input type="email" name="email" placeholder="digite o seu e-mail" />
+                </label>
+                
+                <input type="submit" value="Enviar" />
+
             </form>
 
         </div>
