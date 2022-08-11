808cb9a014924e7aa1d6921a23c09b19223bbe9c
TorugoJS <victor.devfrontend@gmail.com>
2022-08-10 21:07:11 -0300

    Forms

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -12,27 +12,45 @@ const MyForm = () => {
         setName(e.target.value)
     };
 
-    console.log(name);
+    // console.log(name);
+    // console.log(email);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('Enviando dados')
+        console.log(name, email);
+    }
 
 
     return (
         <div>
-
+            {/* 5 - Envio de form */}
             {/* 1 - Criação de form */}
-            <form>
+            <form onSubmit={handleSubmit}>
+
                 <div>
+
                     <label htmlFor="name">Nome:</label>
+
                     <input type="text"
                         name="name"
                         placeholder="Insira seu nome"
                         onChange={handleName}
                     />
+
                 </div>
 
                 {/* Label envolvendo input */}
                 <label>
+
                     <span>E-mail</span>
-                    <input type="email" name="email" placeholder="digite o seu e-mail" />
+                    {/* Simplificação de manipúlção de state */}
+                    <input type="email"
+                        name="email"
+                        placeholder="digite o seu e-mail"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
                 </label>
 
                 <input type="submit" value="Enviar" />
